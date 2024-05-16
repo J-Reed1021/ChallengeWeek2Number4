c846b044ce5cd87fd2b878198ea6cf484de03cd9
joshu <joshu@REEDPC>
2024-05-16 15:20:31 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ChallengeWeek2Number4
+{
+    internal class ChallengeNumber4
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the desired width: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = width; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(number);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
